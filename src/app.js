e3883f8a30e9b812da79c69a80458b8d7d9cab34
ZeroUnoDLC <102610799+ZeroUnoDLC@users.noreply.github.com>
2023-07-18 16:54:23 -0500

    Codigo para backend

--- src/app.js
+++ src/app.js
@@ -9,15 +9,14 @@ const app = express();
 
 
 // configuraciones 
-app.set('url', 'https://esoils.azurewebsites.net');
 
-// const isProduction = process.env.NODE_ENV === 'production';
-// if (isProduction) {
-//   app.set('url', 'https://esoils.azurewebsites.net');
-// } else {
-//   app.set('port', process.env.PORT || 3000);
-//   app.set('url', `http://localhost:${app.get('port')}`);
-// }
+const isProduction = process.env.NODE_ENV === 'production';
+if (isProduction) {
+  app.set('url', 'https://esoils.azurewebsites.net');
+} else {
+  app.set('port', process.env.PORT || 3000);
+  app.set('url', `http://localhost:${app.get('port')}`);
+}
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
