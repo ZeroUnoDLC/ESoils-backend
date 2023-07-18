fe8999f5b76280e535d65abcae89e39dfca333ef
ZeroUnoDLC <102610799+ZeroUnoDLC@users.noreply.github.com>
2023-07-18 15:54:55 -0500

    Al parecer los modulos node si son necesarios.

--- src/app.js
+++ src/app.js
@@ -9,8 +9,13 @@ const app = express();
 
 
 // configuraciones 
-app.set('port', process.env.PORT || 3000);
-app.set('url', `esoils.azurewebsites.net:${app.get('port')}`);
+const isProduction = process.env.NODE_ENV === 'production';
+if (isProduction) {
+  app.set('url', 'https://esoils.azurewebsites.net');
+} else {
+  app.set('port', process.env.PORT || 3000);
+  app.set('url', `http://localhost:${app.get('port')}`);
+}
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
