a47663dc292781cfed4a5923a4697dd04dfbacfe
ZeroUnoDLC <102610799+ZeroUnoDLC@users.noreply.github.com>
2023-07-19 17:46:15 -0500

    funcional 1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from "morgan";
-//import cors from "cors";
+import cors from "cors";
 import homeRoutes from "./routes/home.routes.js";
 
 import bodyParser from 'body-parser';
@@ -19,18 +19,18 @@ const app = express();
 //     app.set('url', `http://localhost:${app.get('port')}`);
 // }
 
+app.set('port', process.env.PORT || 3000);
+app.set('url', `http://localhost:${app.get('port')}`);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 // middlewares
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 // rutas
 app.use(homeRoutes);
-const port = 8080;
-app.listen(port, () => { console.log("localhost:" + port) });
 export default app;
